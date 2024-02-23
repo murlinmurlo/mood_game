97b98e7005c0fb902a34ef54d1b2e1f1ecae8a68
Masha <masha.burdugova@mail.ru>
2024-02-24 01:52:25 +0300

    added function prototypes

--- 20240219/1/prog.py
+++ 20240219/1/prog.py
@@ -13,6 +13,28 @@ def main():
         for b in branches:
             print(os.path.basename(b))
 
+    file = glob.iglob(os.path.join(".git/refs/heads", sys.argv[1])).__next__()
+    with open(file, "r") as f:
+        f_hash = f.read().split()[0]
+
+    while len(list(glob.iglob(os.path.join(".git/objects", f_hash[0:2], f_hash[2:])))):
+        print_last_commit()
+        print_last_commit_tree()
+        print_last_history()
+    
+
+def print_last_commit():
+    pass
+
+def print_last_commit_tree():
+    pass
+
+def print_commit_history():
+    pass
+
+
 
 if __name__ == "__main__":
     main()
+
+
