bd3c7896487be0577ff25f778a6e76f42dcd3015
Masha <masha.burdugova@mail.ru>
2024-02-24 14:41:57 +0300

    print last commit and tree

--- 20240219/1/prog.py
+++ 20240219/1/prog.py
@@ -4,10 +4,10 @@ import glob
 import zlib
 
 def main():
-    if len(argv) == 1:
+    if len(sys.argv) == 1:
         raise Exception("Not a repository")
     
-    elif len(argv) == 2:
+    elif len(sys.argv) == 2:
         path = sys.argv[1]
         branches = glob.iglob(path + '/.git/refs/heads/*')
         for b in branches:
@@ -17,24 +17,30 @@ def main():
     with open(file, "r") as f:
         f_hash = f.read().split()[0]
 
-    while len(list(glob.iglob(os.path.join(".git/objects", f_hash[0:2], f_hash[2:])))):
-        print_last_commit()
-        print_last_commit_tree()
+    while len(list(glob.iglob(os.path.join(".git/objects", f_hash[0:2], f_hash[2:])))) > 0:
+        parent, parent_tree = print_last_commit_tree(f_hash)
         print_last_history()
+
+
+def print_last_commit_tree(commit):
+    file = os.path.join(".git/objects", commit[0:2], commit[2:])
+    with open(file, "rb") as f:
+        data = zlib.decompress(f.read())
     
+    lines = data.split(b"\n")
+    tree_line = next(line for line in lines if line.startswith(b"tree"))
+    parent_line = next(line for line in lines if line.startswith(b"parent"))
+    
+    tree = tree_line.decode().split("tree")[1].strip()
+    parent_tree = parent_line.decode().split("parent")[1].strip()
+    
+    print(f"TREE for commit {commit}")
+    return parent_tree, tree
 
-def print_last_commit():
+
+def print_last_history():
     pass
-
-def print_last_commit_tree():
-    pass
-
-def print_commit_history():
-    pass
-
 
 
 if __name__ == "__main__":
     main()
-
-
