cbb68e0ccd669ab86db6e2dd0df4e744ffd66589
Maria <masha.burdugova@mail.ru>
2024-03-03 20:49:20 +0000

    added check

--- 20240226/2/prog.py
+++ 20240226/2/prog.py
@@ -1,14 +1,14 @@
-import cowsay
+from cowsay import cowsay, list_cows
 import sys
-
+import random
 
 class Player:
     def __init__(self):
         self.x = 0
         self.y = 0
 
-    def encounter(x,y):
-        print(cowsay("Hi!"))
+    def encounter(self):
+        print(cowsay.cow("Hi!"))
 
     def move(self, direction):
         if direction == "up":
@@ -22,10 +22,11 @@ class Player:
 
 
 class Monster:
-    def __init__(self, x, y, hello):
+    def __init__(self, x, y, name, greeting):
         self.x = x
         self.y = y
-        self.hello = hello
+        self.name = name
+        self.greeting = greeting
 
     def encounter(self):
         print(cowsay.cow(self.greeting, cow=self.name))
@@ -50,7 +51,7 @@ class Game:
 
         self.monsters.append(Monster(x, y, name, greeting))
         print(f"Added monster {name} to ({x}, {y}) saying {greeting}")
-            
+
     def run(self):
         while True:
             command = input().strip().split()
@@ -70,8 +71,9 @@ class Game:
                     continue
                 x = int(command[1])
                 y = int(command[2])
-                saying = command[3]
-                self.addmon(x, y, saying)
+                name = command[3]
+                greeting = random.choice(["Hello!", "Don't eat me!!!", "Nice to meet you, white friend!"])
+                self.addmon(x, y, name, greeting)
             else:
                 print("Invalid command")
 
@@ -79,7 +81,7 @@ class Game:
 
             for monster in self.monsters:
                 if monster.x == self.player.x and monster.y == self.player.y:
-                    self.player.encounter()
+                    monster.encounter()
                     break
 
 
