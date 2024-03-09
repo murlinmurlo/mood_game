2e072db4efc48dd37dab4c8fa96a8a570e0d2b30
Maria <masha.burdugova@mail.ru>
2024-03-09 16:30:47 +0000

    added shlex

--- 20240303/1/prog.py
+++ 20240303/1/prog.py
@@ -1,14 +1,14 @@
-from cowsay import cowsay, list_cows
+import cowsay
 import sys
-import random
+import shlex
 
 class Player:
     def __init__(self):
         self.x = 0
         self.y = 0
 
-    def encounter(self):
-        print(cowsay.cow("Hi!"))
+    def encounter(self, x, y):
+        print(cowsay.cow("Hello!"))
 
     def move(self, direction):
         if direction == "up":
@@ -22,39 +22,39 @@ class Player:
 
 
 class Monster:
-    def __init__(self, x, y, name, greeting):
+    def __init__(self, x, y, hello, hp):
         self.x = x
         self.y = y
-        self.name = name
-        self.greeting = greeting
+        self.hello = hello
+        self.hp = hp
 
     def encounter(self):
-        print(cowsay.cow(self.greeting, cow=self.name)) #added name
-
+        print(cowsay.cow(self.hello))
 
 class Game:
     def __init__(self):
         self.player = Player()
         self.monsters = []
 
-    def addmon(self, x, y, name, greeting): #names for monsters
-        if name not in cowsay.list_cows():
-            print("Cannot add unknown monster")
+    def addmon(self, monster_name, hello_string, hp, x, y):
+        try:
+            hp = int(hp)
+        except ValueError:
+            print("Invalid value for hp. Expecting an integer.")
             return
 
-        for monster in self.monsters: #chec
+        for monster in self.monsters:
             if monster.x == x and monster.y == y:
-                monster.name = name
-                monster.greeting = greeting
-                print(f"Monster replaced")
+                monster.hello = hello_string
+                monster.hp = hp
+                print("Monster replaced.")
                 return
-
-        self.monsters.append(Monster(x, y, name, greeting))
-        print(f"Added monster {name} to ({x}, {y}) saying {greeting}")
+        self.monsters.append(Monster(x, y, hello_string, hp))
+        print(f"Added monster at position ({x}, {y}) with greeting: {hello_string}")
 
     def run(self):
         while True:
-            command = input().strip().split()
+            command = shlex.split(input().strip())
             if len(command) == 0:
                 continue
             elif command[0] == "up":
@@ -66,24 +66,24 @@ class Game:
             elif command[0] == "right":
                 self.player.move("right")
             elif command[0] == "addmon":
-                if len(command) != 4:
+                if len(command) != 9:
                     print("Invalid arguments")
                     continue
-                x = int(command[1])
-                y = int(command[2])
-                name = command[3]
-                greeting = random.choice(["Hello!", "Don't eat me!!!", "Nice to meet you, white friend!"])
-                self.addmon(x, y, name, greeting)
+                monster_name = command[1]
+                hello_string = command[3]
+                hp = command[5]
+                x = int(command[7])
+                y = int(command[8])
+                self.addmon(monster_name, hello_string, hp, x, y)
             else:
                 print("Invalid command")
 
-            print(f"Moved to ({self.player.x}, {self.player.y})")
+            print(f"Moved to position ({self.player.x}, {self.player.y})")
 
             for monster in self.monsters:
                 if monster.x == self.player.x and monster.y == self.player.y:
-                    monster.encounter()
+                    self.player.encounter(monster.x, monster.y)
                     break
-
 
 game = Game()
 game.run()
