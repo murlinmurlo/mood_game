be5a9b2983399e36c5cca7fc3db433501fdf1a78
Maria <masha.burdugova@mail.ru>
2024-03-23 21:32:13 +0000

    fixed bugs

--- 20240318/1/server.py
+++ 20240318/1/server.py
@@ -1,5 +1,10 @@
 import socket
 import threading
+import cmd
+import cowsay
+import shlex
+import sys
+import io
 
 class Server:
     def __init__(self, host='localhost', port=12345):
@@ -24,7 +29,10 @@ class Server:
                 break
             command, *args = data.decode().split()
             response = self.execute_command(command, args)
-            client.sendall(response.encode())
+            if response is not None:  # Check if response is not None before sending
+                client.sendall(response.encode())
+
+    
 
     def execute_command(self, command, args):
         if command == 'move':
@@ -39,46 +47,6 @@ class Server:
         else:
             return "Unknown command"
 
-import socket
-import threading
-
-class Server:
-    def __init__(self, host='localhost', port=12345):
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((host, port))
-        self.server.listen(1)
-        self.clients = []
-        self.game_state = GameState()
-
-    def start(self):
-        print("Server started")
-        while True:
-            client, addr = self.server.accept()
-            print(f"Connected by {addr}")
-            self.clients.append(client)
-            threading.Thread(target=self.handle_client, args=(client,)).start()
-
-    def handle_client(self, client):
-        while True:
-            data = client.recv(1024)
-            if not data:
-                break
-            command, *args = data.decode().split()
-            response = self.execute_command(command, args)
-            client.sendall(response.encode())
-
-    def execute_command(self, command, args):
-        if command == 'move':
-            dx, dy = map(int, args)
-            return self.game_state.move(dx, dy)
-        elif command == 'addmon':
-            name, x, y, hello, hp = args
-            return self.game_state.addmon(name, int(x), int(y), hello, int(hp))
-        elif command == 'attack':
-            weapon = args[0]
-            return self.game_state.attack(weapon)
-        else:
-            return "Unknown command"
 
 class GameState:
     def __init__(self):
@@ -93,14 +61,15 @@ class GameState:
         return f"Player moved to position ({new_x}, {new_y})"
 
     def addmon(self, name, x, y, hello, hp):
-        if self.matrix.get((x, y)) is not None:
+        if self.monsters.get((x, y)) is not None:
             print("Replaced the old monster")
 
         if name in cowsay.list_cows() + ["jgsbat"]:
-            self.matrix[(x, y)] = Monster(name, hello, hp)
+            self.monsters[(x, y)] = Monster(name, hello, hp)
             print(f"Added monster {name} to ({x}, {y}) saying {hello} hp {int(hp)}")
         else:
             print("Cannot add unknown monster")
+
 
     def attack(self, weapon):
         for pos, monster in self.monsters.items():
