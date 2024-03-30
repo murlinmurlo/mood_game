8f16e96aa53da3452ad4171affddd3a67890e6bd
Maria <masha.burdugova@mail.ru>
2024-03-30 01:07:37 +0000

    added autentification for users

--- 20240325/1/client.py
+++ 20240325/1/client.py
@@ -1,9 +1,10 @@
 import socket
 
 class Client:
-    def __init__(self, host='localhost', port=12345):
+    def __init__(self, host='localhost', port=12345, username='guest'):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((host, port))
+        self.username = username
 
     def send_command(self, command):
         self.client.sendall(command.encode())
@@ -11,10 +12,17 @@ class Client:
         print(data.decode())
 
     def start(self):
+        self.client.sendall(self.username.encode())
+        response = self.client.recv(1024).decode()
+        print(response)
+        if response != "Connected to the server":
+            print("Connection failed: " + response)
+            return
         while True:
             command = input("> ")
             self.send_command(command)
 
-client = Client()
+print("<<< Welcome to Python-MUD 0.1 >>>")
+username = input("Enter your username: ")
+client = Client(username=username)
 client.start()
-
