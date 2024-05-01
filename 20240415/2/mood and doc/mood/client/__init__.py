b2bcb22827794492fbec787492698d94e198aacc
murlinmurlo <masha.burdugova@mail.ru>
2024-05-01 22:15:51 +0300

    babel

---
+++ 20240415/2/mood and doc/mood/client/__init__.py
@@ -0,0 +1,21 @@
+"""
+Client for MUD (multi user dangeon).
+
+Client command:
+ | ``help``
+ | ``left``
+ | ``right``
+ | ``down``
+ | ``up``
+ | ``addmon``
+ |      ``<name>``
+ |      ``coords <x> <y>``              (non-positional)
+ |      ``hello <message>``             (non-positional)
+ |      ``hp <hp>``                     (non-positional)
+ | ``attack <name> [with weapon]``     (default weapon - sword)
+ | ``sayall <message>``
+ | ``movemonsters on|off``
+ | ``quit``
+
+For more info, see ``help``.
+"""
